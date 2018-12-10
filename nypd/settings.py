b0ddf4026e7e0ccb66f11831f42232aee725faf0
Kaizen Agility <kaizen.agility@gmail.com>
2018-12-10 18:49:23 -0500

    settings

--- nypd/settings.py
+++ nypd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wfieeo14o_!6xyl2t1-ij%r0za2ui=y(z0^_1593sh#8*8n-!p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
